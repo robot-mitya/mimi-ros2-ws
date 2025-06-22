09c9f9152901f74d5119161dba54e7031e22b1fa
DmitryDzz <DzakhovDS@gmail.com>
2025-06-22 23:40:29 +0700

    HeadlightsCmd and ScrAnimCmd messages

--- src/mimi_ble/src/ble_node.cpp
+++ src/mimi_ble/src/ble_node.cpp
@@ -1,8 +1,10 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "mimi_interfaces/msg/drive_cmd.hpp"
 #include "mimi_ble/ble_uart_client.h"
+#include "mimi_interfaces/msg/drive_cmd.hpp"
+#include "mimi_interfaces/msg/scr_anim_cmd.hpp"
+#include "mimi_interfaces/msg/headlights_cmd.hpp"
 
 std::shared_ptr<mimi::BleUartClient> bleClient;
 
@@ -11,11 +13,18 @@ class BleNode final : public rclcpp::Node {
 
     std::string _robotName;
 
-    rclcpp::Subscription<mimi_interfaces::msg::DriveCmd>::SharedPtr driveSubscription_;
+    rclcpp::Subscription<mimi_interfaces::msg::DriveCmd>::SharedPtr driveCmdSubscription_;
+    rclcpp::Subscription<mimi_interfaces::msg::ScrAnimCmd>::SharedPtr scrAnimCmdSubscription_;
+    rclcpp::Subscription<mimi_interfaces::msg::HeadlightsCmd>::SharedPtr headlightsCmdSubscription_;
 
     static int16_t toInt255(double value) {
         value = std::clamp(value, -1.0, 1.0);
         return static_cast<int16_t>(std::round(value * 255.0));
+    }
+
+    static uint8_t toUint255(double value) {
+        value = std::clamp(value, 0.0, 1.0);
+        return static_cast<uint8_t>(std::round(value * 255.0));
     }
 
 public:
@@ -23,16 +32,38 @@ public:
         _robotName = declare_parameter("robot_name", "mimi");
         get_parameter("robot_name", _robotName);
 
-        auto driveTopicCallback = [this](const mimi_interfaces::msg::DriveCmd::UniquePtr &msg) -> void {
+        auto driveCmdTopicCallback = [this](const mimi_interfaces::msg::DriveCmd::UniquePtr &msg) -> void {
             if (bleClient->getState() != mimi::BleUartClient::State::Connected) return;
             const int16_t leftSpeed = toInt255(msg->left_speed);
             const int16_t rightSpeed = toInt255(msg->right_speed);
-            const std::string driveCommand = mimi::str("drv ", leftSpeed, " ", rightSpeed, "\r\n");
-            if (!bleClient->send(driveCommand)) {
-                RCLCPP_ERROR(this->get_logger(), "Sending command failed");
+            const std::string command = mimi::str("drv ", leftSpeed, " ", rightSpeed, "\r\n");
+            if (!bleClient->send(command)) {
+                RCLCPP_ERROR(this->get_logger(), "Sending command failed: %s", command.c_str());
             }
         };
-        driveSubscription_ = this->create_subscription<mimi_interfaces::msg::DriveCmd>("/cmd/drive_cmd", 10, driveTopicCallback);
+
+        auto scrAnimCmdTopicCallback = [this](const mimi_interfaces::msg::ScrAnimCmd::UniquePtr &msg) -> void {
+            if (bleClient->getState() != mimi::BleUartClient::State::Connected) return;
+            const std::string command = mimi::str("emo ", static_cast<int>(msg->id), "\r\n");
+            if (!bleClient->send(command)) {
+                RCLCPP_ERROR(this->get_logger(), "Sending command failed: %s", command.c_str());
+            }
+        };
+
+        auto headlightsCmdTopicCallback = [this](const mimi_interfaces::msg::HeadlightsCmd::UniquePtr &msg) -> void {
+            if (bleClient->getState() != mimi::BleUartClient::State::Connected) return;
+            const int red = toUint255(msg->red);
+            const int green = toUint255(msg->green);
+            const int blue = toUint255(msg->blue);
+            const std::string command = mimi::str("hl ", red, " ", green, " ", blue, "\r\n");
+            if (!bleClient->send(command)) {
+                RCLCPP_ERROR(this->get_logger(), "Sending command failed: %s", command.c_str());
+            }
+        };
+
+        driveCmdSubscription_ = this->create_subscription<mimi_interfaces::msg::DriveCmd>("/cmd/drive_cmd", 10, driveCmdTopicCallback);
+        scrAnimCmdSubscription_ = this->create_subscription<mimi_interfaces::msg::ScrAnimCmd>("/cmd/scr_anim_cmd", 10, scrAnimCmdTopicCallback);
+        headlightsCmdSubscription_ = this->create_subscription<mimi_interfaces::msg::HeadlightsCmd>("/cmd/headlights_cmd", 10, headlightsCmdTopicCallback);
     }
 
     std::string& getRobotName() {
--- src/mimi_common/src/gamepad_node.cpp
+++ src/mimi_common/src/gamepad_node.cpp
@@ -8,23 +8,136 @@
 #include "sensor_msgs/msg/joy.hpp"
 #include "mimi_interfaces/msg/drive_cmd.hpp"
 #include "mimi_interfaces/msg/scr_anim_cmd.hpp"
+#include "mimi_interfaces/msg/headlights_cmd.hpp"
+
+static constexpr int leftJoystickAxisX = 0;
+static constexpr int leftJoystickAxisY = 1;
+static constexpr int leftTriggerAxis = 2;
+// static constexpr int rightJoystickAxisX = 3;
+// static constexpr int rightJoystickAxisY = 4;
+static constexpr int rightTriggerAxis = 5;
+static constexpr int dPadAxisX = 6;
+static constexpr int dPadAxisY = 7;
+static constexpr int buttonA = 0;
+static constexpr int buttonB = 1;
+static constexpr int buttonX = 2;
+static constexpr int buttonY = 3;
+static constexpr int leftBumperButton = 4;
+static constexpr int rightBumperButton = 5;
+// static constexpr int backButton = 6;
+// static constexpr int startButton = 7;
+// static constexpr int guideButton = 8;
+// static constexpr int leftStickButton = 9;
+// static constexpr int rightStickButton = 10;
 
 struct Vector2 {
     float x;
     float y;
+    Vector2() : x(0), y(0) {}
+    Vector2(const float _x, const float _y) : x(_x), y(_y) {}
+};
 
-    Vector2() : x(0), y(0) {
+struct Color3 {
+    float r;
+    float g;
+    float b;
+    Color3() : r(0), g(0), b(0) {}
+    Color3(const float r, const float g, const float b) : r(r), g(g), b(b) {}
+    bool isSame(const float rr, const float gg, const float bb) const {
+        return r == rr && g == gg && b == bb;
+    }
+    bool isSame(const Color3 other) const {
+        return r == other.r && g == other.g && b == other.b;
+    }
+};
+
+using PublishHeadlightsCmdCallback = std::function<void(Color3 color)>;
+
+class HeadlightsManager {
+    PublishHeadlightsCmdCallback publishCallback_ = nullptr;
+    uint8_t pressedButtons_ = 0b00000000;
+    bool headlightsOn_ = false;
+    Color3 srcColor_{1, 1, 1};
+    Color3 msgColor_;
+
+    void processTrigger(const sensor_msgs::msg::Joy::UniquePtr &msg) {
+        if (!headlightsOn_ && !srcColor_.isSame(0, 0, 0)) {
+            const float factor = (1 - msg->axes[leftTriggerAxis]) / 2;
+            const Color3 msgColor(srcColor_.r * factor, srcColor_.g * factor, srcColor_.b * factor);
+            if (!msgColor.isSame(msgColor_)) {
+                msgColor_.r = msgColor.r;
+                msgColor_.g = msgColor.g;
+                msgColor_.b = msgColor.b;
+                publishCallback_(msgColor);
+            }
+        }
     }
 
-    Vector2(const float _x, const float _y) : x(_x), y(_y) {
+    void processButtons(const sensor_msgs::msg::Joy::UniquePtr &msg) {
+        const bool bumperPressed = msg->buttons[leftBumperButton] == 1;
+        const bool pressedA = msg->buttons[buttonA] == 1; // green color
+        const bool pressedB = msg->buttons[buttonB] == 1; // red color
+        const bool pressedX = msg->buttons[buttonX] == 1; // blue color
+        const bool pressedY = msg->buttons[buttonY] == 1; // white color
+        const uint8_t pressedButtons = pressedA << 0 | pressedB << 1 | pressedX << 2 | pressedY << 3;
+
+        std::optional<Color3> newColor;
+        if (pressedButtons != pressedButtons_ && pressedA) {
+            newColor.emplace(0, 1, 0);
+        } else if (pressedButtons != pressedButtons_ && pressedB) {
+            newColor.emplace(1, 0, 0);
+        } else if (pressedButtons != pressedButtons_ && pressedX) {
+            newColor.emplace(0, 0, 1);
+        } else if (pressedButtons != pressedButtons_ && pressedY) {
+            newColor.emplace(1, 1, 1);
+        }
+        pressedButtons_ = pressedButtons;
+
+        if (newColor.has_value()) {
+            if (!bumperPressed || headlightsOn_) {
+                headlightsOn_ = newColor->isSame(srcColor_) ? !headlightsOn_ : true;
+
+                if (publishCallback_) {
+                    const float factor = headlightsOn_ ? 1.0f : 0.0f;
+                    const Color3 msgColor((*newColor).r * factor, (*newColor).g * factor, (*newColor).b * factor);
+                    if (!msgColor.isSame(msgColor_)) {
+                        msgColor_.r = msgColor.r;
+                        msgColor_.g = msgColor.g;
+                        msgColor_.b = msgColor.b;
+                        publishCallback_(msgColor);
+                    }
+                }
+            }
+            srcColor_.r = newColor->r;
+            srcColor_.g = newColor->g;
+            srcColor_.b = newColor->b;
+        }
+    }
+public:
+    void setPublishCallback(PublishHeadlightsCmdCallback publishCallback) {
+        publishCallback_ = std::move(publishCallback);
+    }
+
+    void process(const sensor_msgs::msg::Joy::UniquePtr &msg) {
+        processButtons(msg);
+        processTrigger(msg);
     }
 };
 
 class GamepadNode final : public rclcpp::Node {
     static constexpr double maxSpeed = 1.0;
 
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
-    rclcpp::Publisher<mimi_interfaces::msg::DriveCmd>::SharedPtr publisher_;
+    enum class ControlMode {
+        ScreenAnimations,
+        Headlights,
+    };
+
+    ControlMode controlMode_ = ControlMode::ScreenAnimations;
+
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscriptionJoy_;
+    rclcpp::Publisher<mimi_interfaces::msg::DriveCmd>::SharedPtr publisherDriveCmd_;
+    rclcpp::Publisher<mimi_interfaces::msg::ScrAnimCmd>::SharedPtr publisherScrAnimCmd_;
+    rclcpp::Publisher<mimi_interfaces::msg::HeadlightsCmd>::SharedPtr publisherHeadlightsCmd_;
 
     static Vector2 CircleToSquareInFirstQuadrant(Vector2 value) {
         float x = value.x;
@@ -83,26 +196,118 @@ class GamepadNode final : public rclcpp::Node {
         return {static_cast<float>(leftSpeed), static_cast<float>(rightSpeed)};
     }
 
+    static bool approximatelyEqual(const float a, const float b) {
+        return std::fabs(a - b) < 1e-3f;
+    }
+
+    static constexpr int scrAnimEmpty = 0;
+    static constexpr int scrAnimBluetooth = 1;
+    static constexpr int scrAnimYes = 2;
+    static constexpr int scrAnimNo = 3;
+    static constexpr int scrAnimHeart = 4;
+    static constexpr int scrAnimSpinner = 5;
+    static constexpr int scrAnimNormalFace = 6;
+    static constexpr int scrAnimHappyFace = 7;
+    static constexpr int scrAnimSadFace = 8;
+    static constexpr int scrAnimAngryFace = 9;
+    static constexpr int scrAnimTalkingFace = 10;
+
+    Color3 headlightsColor_;
+    bool headlightsOn_ = false;
+
+    mimi_interfaces::msg::ScrAnimCmd scrAnimMsg_;
+    mimi_interfaces::msg::DriveCmd driveMsg_;
+
+    void processControlModeSwitching(const sensor_msgs::msg::Joy::UniquePtr &msg) {
+        if (approximatelyEqual(msg->axes[dPadAxisX], -1)) { // D-PAD RIGHT
+            if (controlMode_ != ControlMode::Headlights) {
+                controlMode_ = ControlMode::Headlights;
+                RCLCPP_INFO(this->get_logger(), "Switched to ControlMode::Headlights");
+            }
+        } else if (approximatelyEqual(msg->axes[dPadAxisY], 1)) { // D-PAD UP
+            if (controlMode_ != ControlMode::ScreenAnimations) {
+                controlMode_ = ControlMode::ScreenAnimations;
+                RCLCPP_INFO(this->get_logger(), "Switched to ControlMode::ScreenAnimations");
+            }
+        }
+    }
+
+    void processScreenAnimationCommands(const sensor_msgs::msg::Joy::UniquePtr &msg) {
+        if (controlMode_ != ControlMode::ScreenAnimations) return;
+        const bool bumperPressed = msg->buttons[leftBumperButton] == 1;
+        const bool pressedA = msg->buttons[buttonA] == 1;
+        const bool pressedB = msg->buttons[buttonB] == 1;
+        const bool pressedX = msg->buttons[buttonX] == 1;
+        const bool pressedY = msg->buttons[buttonY] == 1;
+        std::optional<uint32_t> scrAnimId;
+        if (bumperPressed) {
+            if (pressedA) {
+                scrAnimId = scrAnimHeart;
+            } else if (pressedB) {
+                scrAnimId = scrAnimAngryFace;
+            } else if (pressedX) {
+                scrAnimId = scrAnimSpinner;
+            } else if (pressedY) {
+                scrAnimId = scrAnimNo;
+            }
+        } else {
+            if (pressedA) {
+                scrAnimId = scrAnimHappyFace;
+            } else if (pressedB) {
+                scrAnimId = scrAnimSadFace;
+            } else if (pressedX) {
+                scrAnimId = scrAnimTalkingFace;
+            } else if (pressedY) {
+                scrAnimId = scrAnimYes;
+            }
+        }
+        if (scrAnimId.has_value() && scrAnimMsg_.id != *scrAnimId) {
+            scrAnimMsg_.id = *scrAnimId;
+            publisherScrAnimCmd_->publish(scrAnimMsg_);
+        }
+    }
+
+    HeadlightsManager headlightsManager_;
+
+    void processDriveCommands(const sensor_msgs::msg::Joy::UniquePtr &msg) {
+        const Vector2 joystickVector(-msg->axes[leftJoystickAxisX], msg->axes[leftJoystickAxisY]);
+        const Vector2 moveVector = CircleToSquare(joystickVector);
+        const bool fastRotation = msg->buttons[rightBumperButton] > 0;
+        const double speedFactor = fastRotation ? 1.0 : (1 - msg->axes[rightTriggerAxis]) / 2;
+        const Vector2 motorValues = GetMotorValues(moveVector, static_cast<float>(speedFactor), fastRotation);
+
+        // driveMsg_.left_speed = motorValues.x;
+        // driveMsg_.right_speed = motorValues.y;
+        // publisherDriveCmd_->publish(driveMsg_);
+        if (!approximatelyEqual(driveMsg_.left_speed, motorValues.x) || !approximatelyEqual(driveMsg_.right_speed, motorValues.y)) {
+            driveMsg_.left_speed = motorValues.x;
+            driveMsg_.right_speed = motorValues.y;
+            publisherDriveCmd_->publish(driveMsg_);
+        }
+    }
 public:
-    GamepadNode()
-        : Node("gamepad_node") {
+    GamepadNode() : Node("gamepad_node") {
+        this->headlightsManager_.setPublishCallback([this] (const Color3 color) {
+            mimi_interfaces::msg::HeadlightsCmd message;
+            message.red = color.r;
+            message.green = color.g;
+            message.blue = color.b;
+            publisherHeadlightsCmd_->publish(message);
+        });
+
         auto joyTopicCallback = [this](const sensor_msgs::msg::Joy::UniquePtr &msg) -> void {
-            const Vector2 joystickVector(-msg->axes[0], msg->axes[1]);          // Left joystick
-            const Vector2 moveVector = CircleToSquare(joystickVector);
-            const bool fastRotation = msg->buttons[5] > 0;                          // Right shoulder
-            const double speedFactor = fastRotation ? 1.0 : (1 - msg->axes[5]) / 2; // Right trigger
-            const Vector2 motorValues = GetMotorValues(moveVector, static_cast<float>(speedFactor), fastRotation);
-            // RCLCPP_INFO(this->get_logger(), "Left joystick: [%4.1f, %4.1f], speed: [%4.1f]", msg->axes[0], msg->axes[1], msg->axes[5]);
-            // RCLCPP_INFO(this->get_logger(), "Move vector: [%4.1f, %4.1f], speedFactor: [%4.1f], fastRotation: [%d]", moveVector.x, moveVector.y, speedFactor, fastRotation);
-            mimi_interfaces::msg::DriveCmd driveMsg;
-            driveMsg.left_speed = motorValues.x;
-            driveMsg.right_speed = motorValues.y;
-            publisher_->publish(driveMsg);
-            // RCLCPP_INFO(this->get_logger(), "Motor values: [%4.1f, %4.1f]", driveMsg.left_speed, driveMsg.right_speed);
+            this->processControlModeSwitching(msg);
+            this->processScreenAnimationCommands(msg);
+            if (controlMode_ == ControlMode::Headlights) {
+                this->headlightsManager_.process(msg);
+            }
+            this->processDriveCommands(msg);
         };
-        publisher_ = this->create_publisher<mimi_interfaces::msg::DriveCmd>("/cmd/drive_cmd", 10);
-        subscription_ =
-                this->create_subscription<sensor_msgs::msg::Joy>("/joy", 10, joyTopicCallback);
+
+        publisherDriveCmd_ = this->create_publisher<mimi_interfaces::msg::DriveCmd>("/cmd/drive_cmd", 10);
+        publisherScrAnimCmd_ = this->create_publisher<mimi_interfaces::msg::ScrAnimCmd>("/cmd/scr_anim_cmd", 10);
+        publisherHeadlightsCmd_ = this->create_publisher<mimi_interfaces::msg::HeadlightsCmd>("/cmd/headlights_cmd", 10);
+        subscriptionJoy_ = this->create_subscription<sensor_msgs::msg::Joy>("/joy", 10, joyTopicCallback);
     }
 };
 
